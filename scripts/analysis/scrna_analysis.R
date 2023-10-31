f49b9f6d3867ffa71a81730fe73194c07b862dd6
jackaloppy <ranwei@arizona.edu>
2023-10-30 17:01:05 -0700

    updating sample outputs for c1

--- scripts/analysis/scrna_analysis.R
+++ scripts/analysis/scrna_analysis.R
@@ -1,9 +1,32 @@
-## SoupX (need to download whole /outs folder)
-sc_m1 = load10X("./data/c1_scrna_filtered_feature_bc_matrix")
+### SoupX (need to download whole /outs folder)
+### Don't need to do it now. It's a refined process that we could do later to better clean up the dataset.
+### Same as DoubletFinder
+#test_c1 <- load10X("./data/c1_scrna_outs")
+#test_c1 <- autoEstCont(test_c1)
+#test_c1 <- adjustCounts(test_c1)
+### No need to use SoupX because estimated global rho is 0.03, which is low.
 
+
+## Integrative analysis in Seurat V5
+options(Seurat.object.assay.version = "v5")
 
 c1_scrna <- Read10X("./data/c1_scrna_filtered_feature_bc_matrix/")
-c1_scrna <- CreateSeuratObject(counts = c1_scrna, project = "c1_scrna", min.cells = 3, min.features=200)
+c1_scrna <- CreateSeuratObject(c1_scrna,min.cells = 3, min.features = 200)
+c1_scrna$sample <- "c1"
+c2_scrna <- Read10X("./data/c2_scrna_filtered_feature_bc_matrix/")
+c2_scrna <- CreateSeuratObject(c2_scrna,min.cells = 3, min.features = 200)
+c2_scrna$sample <- "c2"
+t1_scrna <- Read10X("./data/t1_scrna_filtered_feature_bc_matrix/")
+t1_scrna <- CreateSeuratObject(t1_scrna,min.cells = 3, min.features = 200)
+t1_scrna$sample <- "t1"
+t2_scrna <- Read10X("./data/t2_scrna_filtered_feature_bc_matrix/")
+t2_scrna <- CreateSeuratObject(t2_scrna,min.cells = 3, min.features = 200)
+t2_scrna$sample <- "t2"
+
+lupus.anchors <- FindIntegrationAnchors(object.list=list(c1_scrna,c2_scrna,t1_scrna,t2_scrna))
+combined <- IntegrateData(anchorset=lupus.anchors)
+DefaultAssay(combined) <- "integrated"
+
 
 c1_scrna[["percent.mt"]] <- PercentageFeatureSet(c1_scrna, pattern = "^MT-")
 VlnPlot(c1_scrna, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
